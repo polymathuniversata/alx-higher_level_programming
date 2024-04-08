a1bc69601174037e02d523967db2f9dc96ea15d3
Polymath Universata <polymathuniversata@outlook.com>
2024-04-08 12:41:41 +0300

    Unix file formart fix

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,6 @@
+#!usr/bin/node
+let firstLine ="C is fun", 
+    secondLine = "Python is cool",
+    thirdline = "Javascript is amazing"; 
+    
+console.log(firstLine + "\n" + secondLine + "\n" + thirdline);
