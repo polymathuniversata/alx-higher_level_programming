ebe69f9d5ff28d7b667dbc1713eb0d1e7d533414
polymathuniversata <polymathuniversata@outlook.com>
2024-04-11 10:33:03 -0700

    fix execution errors

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,14 +6,12 @@ import sys
 import MySQLdb
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(host="localhsost", user=sys.argv[1], passwd=sys.argv[2],
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
                          db=sys.argv[3], port=3306)
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()
-    
+    cur.execute("SELECT * FROM states;")
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)
